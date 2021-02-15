1bd3e7abd4f9c0af5e8eb448feddeb7dc8612eb1
ADStrafe <vojtech.lahuta@gmail.com>
2021-02-15 01:21:04 +0100

    inital commit

---
+++ lib/src/main/java/local/kurvodej/plugins/PluginsPlugin.java
@@ -0,0 +1,10 @@
+package local.kurvodej.plugins;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PluginsPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("yeet");
+    }
+}
